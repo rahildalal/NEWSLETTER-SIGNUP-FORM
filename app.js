4c3912ca20c32a1580b23ad716cc6983bf538932
rahildalal <rahildalal3@gmail.com>
2022-06-16 14:58:24 -0400

    Upgraded Bootstrap ver & added relevant comments

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
+//imports
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
 const https = require("https");
+
+//set env for API key
 require('dotenv').config()
 
+//run express app
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true})); //ask app to get data from html using body-parser
 
+//implement get request for signup page
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
+//implement post request for signup page to receive data entered by user
 app.post("/", function(req, res) {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
@@ -31,15 +37,17 @@ app.post("/", function(req, res) {
     ]
   }
 
+  //convert JS object to JSON string for sending data to mailchimp
   const jsonData = JSON.stringify(data);
 
-  const url = "https://us11.api.mailchimp.com/3.0/lists/"+process.env.LIST_ID;
+  const url = "https://us11.api.mailchimp.com/3.0/lists/"+process.env.LIST_ID; //API url
 
   const options = {
     method: "POST",
     auth: "rahil:"+process.env.API_KEY
   }
 
+  //send request with data to api
   const request = https.request(url, options, function(response) {
 
     if (response.statusCode === 200) {
@@ -58,12 +66,12 @@ app.post("/", function(req, res) {
 
 });
 
+//implement failure route
 app.post("/failure", function(req, res) {
   res.redirect("/")
 });
 
-
-
+//set local port
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is up!!");
 });
